0f821143293dc1f5c3091e04228b49d39708b5c3
Motorheadbanger <8697534+Motorheadbanger@users.noreply.github.com>
2023-10-27 23:41:58 +0200

    feat: Add functions to call operation and update values

--- js.js
+++ js.js
@@ -1,6 +1,11 @@
 let x;
 let y;
 let operator;
+let current = 0;
+let numberButton = document.querySelector(".number");
+let display = document.querySelector("#display");
+
+numberButton.addEventListener('click', event => onNumberClick(event.target));
 
 function add(x, y) {
     return x + y;
@@ -16,4 +21,31 @@ function multiply(x, y) {
 
 function divide(x, y) {
     return x / y;
+}
+
+function operate() {
+    switch (operator) {
+        case '+':
+            add(x, y);
+            break;
+        case '-':
+            subtract(x, y);
+            break;
+        case '*':
+            multiply(x, y);
+            break;
+        case '/':
+            divide(x, y);
+            break;
+        default:
+            alert('Unrecognized operator');
+    }
+}
+
+function onNumberClick(button) {
+    let char = button.textContent;
+    let nmbr = parseInt(char);
+
+    display.textContent += char;
+    current = current * 10 + nmbr;
 }
